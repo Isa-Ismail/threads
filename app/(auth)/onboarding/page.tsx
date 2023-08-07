5a071095463b93acddb3c442f4177c9e8a7c1aee
isa-ismail <fahimismail75.scc@gmail.com>
2023-08-07 22:18:54 +0600

    sense

---
+++ app/(auth)/onboarding/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+interface Props {}
+
+const page = () => {
+  return <div>Onboarding</div>
+}
+
+export default page
--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default async function Home() {
-  const res = await fetch('https://ict-6.vercel.app/api/auth')
-  const data = await res.json()
-  return (
-    <main>
-      <h1>Next.js { JSON.stringify(data) }</h1>
-    </main>
-  )
-}
