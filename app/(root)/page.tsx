cf542f39fec615f8c9f42edd2f32a01c36ff1ffa
isa-ismail <fahimismail75.scc@gmail.com>
2023-09-14 02:39:11 +0600

    apnea

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,52 @@
 //app/page.tsx
+'use client';
+
+import { sequence } from "@/constant";
+import React from "react";
 
 export default function Home() {
+  
+  const [s, setS] = React.useState("'A'")
+
+function findRanges(arr:string[]) {
+    let ranges = [];
+    let start = null;
+    let end = null;
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === s) {
+            if (start === null) {
+                start = i + 1;
+            }
+            end = i + 1;
+        } else if (start !== null) {
+            ranges.push(`${start}:${end}`);
+            start = null;
+            end = null;
+        }
+    }
+
+    if (start !== null) {
+        ranges.push(`${start}:${end}`);
+    }
+
+    return ranges.join(', '); // Join the ranges with commas
+}
+
+const sequenceArray = sequence.trim().split('\n').map(item => item.trim());
+
+const ranges = findRanges(sequenceArray);
+
+console.log(ranges);
+
+console.log(sequenceArray);
+
+
+
   return (
     <div>
-      <h1 className='text-heading4-medium text-light-1'>Threads</h1>
+      <button className="bg-white p-4 rounded-md" onClick={()=> s==="'A'"?setS("'N'"):setS("'A'")}>toggle</button>
+      <p className="text-white">{s==="'A'"?'Apnea':'Non Apnea' }<br/>{ranges}</p>
     </div>
   )
 }
